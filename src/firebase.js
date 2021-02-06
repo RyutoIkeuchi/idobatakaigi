d856fd0b4bdd5b467b4a757767dbd15c2dcc3bba
Ikeuchi Ryuto <ryu.i7133@gmail.com>
2021-02-06 15:31:14 +0900

    remove hard coded env vars

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,21 @@
 import firebase from 'firebase';
+
+const {
+  REACT_APP_FIREBASE_API_KEY,
+  REACT_APP_FIREBASE_AUTH_DOMAIN,
+  REACT_APP_FIREBASE_PROJECT_ID,
+  REACT_APP_FIREBASE_STORAGE_BUCKET,
+  REACT_APP_FIREBASE_MESSAGEING_SENDER_ID,
+  REACT_APP_FIREBASE_APP_ID,
+} = process.env;
   
 const firebaseConfig = {
-    apiKey: "AIzaSyA4iCLdMweezLy-7FX7IgqZXbmdodcIHA4",
-    authDomain: "idobatakaigi-with-ri.firebaseapp.com",
-    projectId: "idobatakaigi-with-ri",
-    storageBucket: "idobatakaigi-with-ri.appspot.com",
-    messagingSenderId: "263534605793",
-    appId: "1:263534605793:web:71d29b16cd9f6cae550ea0"
+  apiKey: REACT_APP_FIREBASE_API_KEY,
+  authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId:  REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: REACT_APP_FIREBASE_MESSAGEING_SENDER_ID,
+  appId: REACT_APP_FIREBASE_APP_ID,
   };
 firebase.initializeApp(firebaseConfig);
 const database = firebase.database();
